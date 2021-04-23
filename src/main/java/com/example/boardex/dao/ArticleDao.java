5c4b44bf4172e0eb6a21bcdd37242cb24fa13a0e
yoojaein10 <t_5000@naver.com>
2021-04-23 17:54:01 +0900

    DB 연동 중

--- src/main/java/com/example/boardex/dao/ArticleDao.java
+++ src/main/java/com/example/boardex/dao/ArticleDao.java
@@ -2,8 +2,11 @@ package com.example.boardex.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.boardex.dto.Article;
 
+@Mapper // 이렇게 해주면 ArticleDao의 구현체를 마이바티스가 대신 구현해줌
 public interface ArticleDao {
 	
 	public List<Article> getList();
--- src/main/java/com/example/boardex/dto/Article.java
+++ src/main/java/com/example/boardex/dto/Article.java
@@ -9,10 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Article {
 	
-	public Article(int i, String string, String string2, String string3, String string4, String string5,
-			String string6) {
-		// TODO Auto-generated constructor stub
-	}
+//	public Article(int i, String string, String string2, String string3, String string4, String string5,
+//			String string6) {
+//		// TODO Auto-generated constructor stub
+//	}
 	
 	public int id;
 	public String Subject;
